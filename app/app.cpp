f4922d500b6945309b5dd5268fcd70727aa30279
gen-how <billchen85@gmail.com>
2023-05-30 14:45:38 +0800

    refactor: Update macros
    - Add new macros for showing source code informations.

--- app/app.cpp
+++ app/app.cpp
@@ -1,12 +1,24 @@
+#include <cstdio>
+
 #include "CppLogger/logger.h"
 
 int main(int argc, char* argv[]) {
-  Logger::Trace("This is a Trace message.");
-  Logger::Debug("This is a Debug message.");
-  Logger::Info("This is an Info message.");
-  Logger::Warn("This is a Warn message.");
-  Logger::Error("This is an Error message.");
-  Logger::Fatal("This is a Fatal message.");
-  LOG_TRACE("This is a Trace message with source code information.");
+  // These logs will always be printed.
+  Logger::Log(LogLevel::kTRACE, "This is a trace message.");
+  Logger::Log(LogLevel::kDEBUG, "This is a debug message.");
+  Logger::Log(LogLevel::kINFO, "This is an info message.");
+  Logger::Log(LogLevel::kWARN, "This is a warning message.");
+  Logger::Log(LogLevel::kERROR, "This is an error message.");
+  Logger::Log(LogLevel::kFATAL, "This is a fatal message.");
+  puts("============================================================");
+
+  // These logs will be printed only if ENABLE_LOG_LEVEL is higher than the
+  // corresponding log level.
+  LOG_TRACE("This is a trace message with source code information.");
+  LOG_DEBUG("This is a debug message with source code information.");
+  LOG_INFO("This is an info message with source code information.");
+  LOG_WARN("This is a warning message with source code information.");
+  LOG_ERROR("This is an error message with source code information.");
+  LOG_FATAL("This is a fatal message with source code information.");
   return 0;
 }
